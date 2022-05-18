9c7749444edce6533522eb035bf31cdbb43976fc
katliebig <liebig.kat@gmail.com>
2022-05-18 22:38:38 +0200

    countdown app

--- README.md
+++ README.md
@@ -1,27 +1,14 @@
-# NaturalCyclesChallenge
+## NaturalCyclesChallenge
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.0.4.
+# Setup
 
-## Development server
+This project is running with Angular v13.0.3 and TypeScript v4.4.4. To set it up, make sure you have Node.js including npm installed. Run `npm install -g @angular/cli` to install Angular globally, navigate into the project's src folder and run `npm install`. Then run the project using `ng serve`.
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+# Comments
 
-## Code scaffolding
+This projects was developed for the Natural Cycles Frontend Challenge.
+What could be improved here is validation of user input, especially for the date picker. The color contrast is not WCAG-compliant. I would prefer not having to use setTimeout for the text-fit solution. If this was a larger project, I would also extract functionalities into smaller, reusable components.
 
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
+# View it live
 
-## Build
-
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+https://nc-challenge-katliebig.netlify.app/
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,96 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'natural-cycles-challenge';
+export class AppComponent implements OnInit, AfterViewInit {
+  ngOnInit(): void {
+    if (Number(localStorage.getItem('date'))) {
+      this.date = new Date(Number(localStorage.getItem('date')))
+    }
+  }
+  ngAfterViewInit() {
+    if (this.title)
+      this.fitText(document.querySelector('#title') as HTMLElement)
+    if (this.date) {
+      this.getCountdown()
+      setTimeout(
+        () => this.fitText(document.querySelector('#date') as HTMLElement),
+        500,
+      )
+    }
+  }
+
+  title = localStorage.getItem('title')
+  date: Date | null = null
+  tomorrow = new Date().getTime() + 1000 * 60 * 60 * 24
+
+  fitText(el: HTMLElement) {
+    el.style.fontSize = '1vw'
+    const elWidth = el.getBoundingClientRect().width
+    if (elWidth < window.innerWidth) {
+      el.style.fontSize = `${(window.innerWidth - 60) / elWidth}vw`
+    }
+  }
+
+  onTitleInput(event: Event) {
+    const input = event.target as HTMLInputElement
+    this.title = input.value
+    localStorage.setItem('title', this.title)
+    setTimeout(() => {
+      const el = document.querySelector('#title')
+      if (el) this.fitText(el as HTMLElement)
+    }, 10)
+  }
+
+  onDateInput(event: Event) {
+    const pristine = !this.date
+    const input = event.target as HTMLInputElement
+    if (input.value) {
+      this.date = new Date(input.value)
+      localStorage.setItem('date', this.date.getTime().toString())
+
+      this.getCountdown()
+      setTimeout(() => {
+        const el = document.querySelector('#date')
+        if (el) {
+          if (pristine) {
+            setTimeout(() => this.fitText(el as HTMLElement), 500)
+          } else {
+            this.fitText(el as HTMLElement)
+          }
+        }
+      }, 10)
+    } else {
+      this.date = null
+      localStorage.removeItem('date')
+    }
+  }
+
+  getCountdown() {
+    const id = setInterval(() => {
+      if (this.date) {
+        const now = new Date().getTime()
+        const diff = this.date.getTime() - now
+
+        const days = Math.floor(diff / (1000 * 60 * 60 * 24))
+        const hours = Math.floor(
+          (diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+        )
+        const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60))
+        const seconds = Math.floor((diff % (1000 * 60)) / 1000)
+
+        const dateElement = document.querySelector('#date')
+        if (dateElement) {
+          dateElement.innerHTML = `${days} days, ${hours}h, ${minutes}m, ${seconds}s`
+          if (diff < 0) {
+            dateElement.innerHTML = "Hooray! Time's up!"
+            clearInterval(id)
+          }
+        }
+      }
+    }, 500)
+  }
 }
